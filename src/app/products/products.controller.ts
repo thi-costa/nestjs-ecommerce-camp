22f0971da47fae8b50833fa476d2bb99e9807af8
Thiago Costa <thiago14abr@gmail.com>
2022-02-28 22:34:01 -0300

    Routes for products finished

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -1,8 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Logger,
+  Param,
+  Patch,
   Post,
   Query,
   Request,
@@ -10,10 +13,11 @@ import {
 } from '@nestjs/common';
 import { CreateProductDto } from '@app/products/dto/create-products.dto';
 import { GetProductsFilterDto } from '@app/products/dto/get-products-filter.dto';
-import { Product } from '@app/products/products.entity';
+import { Product } from '@shared/entities/products.entity';
 import { ProductsService } from '@app/products/products.service';
 import { JwtGuard } from '@auth/guards/jwt.guard';
 import { IsAdminGuard } from '@auth/guards/is-admin.guard';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -25,6 +29,11 @@ export class ProductsController {
     this.logger.debug('GetProducts called');
 
     return this.productsService.getProducts(filterDto);
+  }
+
+  @Get('/:id')
+  getProductById(@Param('id') id: string): Promise<Product> {
+    return this.productsService.getProductById(id);
   }
 
   @UseGuards(JwtGuard, IsAdminGuard)
@@ -39,5 +48,23 @@ export class ProductsController {
       }", created a new product. Data: ${JSON.stringify(createProductDto)}"`,
     );
     return this.productsService.createProduct(createProductDto, req.user);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updateProductDto: object,
+  ): Promise<Product> {
+    this.logger.verbose(
+      `Update DTO object ${JSON.stringify(updateProductDto)}`,
+    );
+    return this.productsService.updateProduct(id, updateProductDto);
+  }
+
+  @UseGuards(JwtGuard, IsAdminGuard)
+  @Delete('/:id')
+  deleteProduct(@Param('id') id: string): Promise<void> {
+    this.logger.debug('Method deleteProduct called');
+    return this.productsService.deleteProduct(id);
   }
 }
